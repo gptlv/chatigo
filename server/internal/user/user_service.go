cbc95844b64aac42d3276a83d34d1ac9e812d4b6
gptlv <usernamegg@mail.ru>
2024-01-03 19:16:24 +0300

    add: login & logout endpoints

--- server/internal/user/user_service.go
+++ server/internal/user/user_service.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/gptlv/chatigo/server/util"
 )
 
@@ -12,6 +13,14 @@ type service struct {
 	Repository
 	timeout time.Duration
 }
+
+type MyJWTClaims struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+const secretKey = "secret"
 
 func NewService(repository Repository) Service {
 	return &service{
@@ -49,4 +58,38 @@ func (s *service) CreateUser(c context.Context, req *CreateUserReq) (*CreateUser
 	}
 
 	return res, nil
+}
+
+func (s *service) Login(c context.Context, req *LoginUserReq) (*LoginUserRes, error) {
+	ctx, cancel := context.WithTimeout(c, s.timeout)
+	defer cancel()
+
+	u, err := s.Repository.GetUserByEmail(ctx, req.Email)
+
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	err = util.CheckPasswordWithHash(req.Password, u.Password)
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, MyJWTClaims{
+		ID:       strconv.Itoa(int(u.ID)),
+		Username: u.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    strconv.Itoa(int(u.ID)),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
+		},
+	})
+
+	ss, err := token.SignedString([]byte(secretKey))
+
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	return &LoginUserRes{accessToken: ss, Username: u.Username, ID: strconv.Itoa(int(u.ID))}, nil
+
 }
